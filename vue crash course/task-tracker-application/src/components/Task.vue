b9b9a52d897d6dc53127688312f7950e5233900f
ryan4reed <ryan4reed@gmail.com>
2022-11-14 14:39:46 +0200

    Vue crash course Traversy Media init commit.

---
+++ vue crash course/task-tracker-application/src/components/Task.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="task">
+        <h3>{{ task.text }}</h3>
+        <p>{{ task.day }}</p>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'Task',
+        props: {
+            task: Object
+        }
+    }
+</script>
+
+<style>
+.task {
+    background-color: #eeebeb;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+.task.reminder {
+    border-left: green;
+}
+
+
+h3 {
+    display: flex;
+    align-items: center;
+}
+</style>
